9d08fbcdcb1cc0015316b3d2470f7a86917ac2d2
jessykate <jessy@jessykate.com>
2016-12-26 19:45:58 -0800

    fix collision between goal index and detail page; allow html on goal detail page.

--- goals/urls.py
+++ goals/urls.py
@@ -6,5 +6,5 @@ app_name = 'goals'
 urlpatterns = [
     url(r'^(?P<year>[0-9]+)/$', views.goal_list, name='goal_list'),
     url(r'^create$', views.create, name='create'),
-    url(r'^(?P<goals_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^detail/(?P<goals_id>[0-9]+)/$', views.detail, name='detail'),
 ]
