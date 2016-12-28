713efb5db89a7a9f6f9d28876616129df4340be2
jessykate <jessy@jessykate.com>
2016-12-28 10:38:26 -0800

    heroku requirements

--- dandelion/local_settings.py
+++ dandelion/local_settings.py
@@ -4,6 +4,10 @@ SECRET_KEY = 'a9cky6msp%i37(*w%_bkfki!cm5vgn)v$f*b%%a7kdx=pcfzg+'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
--- dandelion/settings.py
+++ dandelion/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '4D.herokuapp.com']
 
 
 # Application definition
@@ -106,13 +106,23 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+            os.path.join(PROJECT_ROOT, 'static'),
+            )
 
 
 LOGIN_URL = '/user/login/'
 LOGOUT_URL = '/user/logout/'
 LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # import any local settings
 try:
