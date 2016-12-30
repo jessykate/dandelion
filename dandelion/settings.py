656426a23794980bd1b7f208be8b4f696e663876
jessykate <jessy@jessykate.com>
2016-12-30 09:10:35 -0800

    Using whitenoise in dev as well...

--- dandelion/settings.py
+++ dandelion/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 	'dandelion',
 	'newyears',
